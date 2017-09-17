196504e572bb6579611a9162287f346f067ed66a
drginm <drginm>
2017-09-17 15:43:02 -0500

    Reordered the listen and the last method to keep the code better organized

--- node-express-mongo/web-site/index.js
+++ node-express-mongo/web-site/index.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
 app.get('/testFind', function (req, res) {
   database.Kitten.find(function (err, kittens) {
     if (err) return res.error(err);
     console.log(kittens);
     res.json(kittens);
   })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
